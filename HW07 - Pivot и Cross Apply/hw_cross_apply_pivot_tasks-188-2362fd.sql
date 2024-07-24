bf358a5cd93de02da4b94fce9b1587a70385b0fb
SergeiGorshenin <gorsheninsn@mail.ru>
2024-07-24 20:26:28 +0300

    HW_07
    HW_07_01

---
+++ HW07 - Pivot и Cross Apply/hw_cross_apply_pivot_tasks-188-2362fd.sql
@@ -0,0 +1,174 @@
+/*
+Домашнее задание по курсу MS SQL Server Developer в OTUS.
+
+Занятие "05 - Операторы CROSS APPLY, PIVOT, UNPIVOT".
+
+Задания выполняются с использованием базы данных WideWorldImporters.
+
+Бэкап БД можно скачать отсюда:
+https://github.com/Microsoft/sql-server-samples/releases/tag/wide-world-importers-v1.0
+Нужен WideWorldImporters-Full.bak
+
+Описание WideWorldImporters от Microsoft:
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-what-is
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-oltp-database-catalog
+*/
+
+-- ---------------------------------------------------------------------------
+-- Задание - написать выборки для получения указанных ниже данных.
+-- ---------------------------------------------------------------------------
+
+USE WideWorldImporters
+
+/*
+1. Требуется написать запрос, который в результате своего выполнения 
+формирует сводку по количеству покупок в разрезе клиентов и месяцев.
+В строках должны быть месяцы (дата начала месяца), в столбцах - клиенты.
+
+Клиентов взять с ID 2-6, это все подразделение Tailspin Toys.
+Имя клиента нужно поменять так чтобы осталось только уточнение.
+Например, исходное значение "Tailspin Toys (Gasport, NY)" - вы выводите только "Gasport, NY".
+Дата должна иметь формат dd.mm.yyyy, например, 25.12.2019.
+
+Пример, как должны выглядеть результаты:
+-------------+--------------------+--------------------+-------------+--------------+------------
+InvoiceMonth | Peeples Valley, AZ | Medicine Lodge, KS | Gasport, NY | Sylvanite, MT | Jessie, ND
+-------------+--------------------+--------------------+-------------+--------------+------------
+01.01.2013   |      3             |        1           |      4      |      2        |     2
+01.02.2013   |      7             |        3           |      4      |      2        |     1
+-------------+--------------------+--------------------+-------------+--------------+------------
+*/
+
+drop table if exists #invoices
+
+select 
+	REPLACE(REPLACE(Customers.CustomerName, 'Tailspin Toys (', ''), ')', '') as CustomerName,
+	DATETRUNC(month, Invoices.InvoiceDate) as InvoiceMonth,
+	count(Invoices.InvoiceID) as CountInvoiceID
+into #invoices
+from Sales.Invoices as Invoices
+left join Sales.Customers as Customers
+	on Invoices.CustomerID = Customers.CustomerID
+where Invoices.CustomerID between 2 and 6
+group by REPLACE(REPLACE(Customers.CustomerName, 'Tailspin Toys (', ''), ')', ''), DATETRUNC(month, Invoices.InvoiceDate)
+;
+with pivot_cte as(
+	select 
+		InvoiceMonth,
+		[Gasport, NY], [Jessie, ND], [Medicine Lodge, KS], [Peeples Valley, AZ], [Sylvanite, MT]
+	from 
+	(select InvoiceMonth, CustomerName, CountInvoiceID from #invoices)
+	as SourceTable
+	pivot
+	(
+	max(CountInvoiceID)
+	for CustomerName
+	in ([Gasport, NY], [Jessie, ND], [Medicine Lodge, KS], [Peeples Valley, AZ], [Sylvanite, MT])
+	)
+	as PivotTable
+)
+
+select
+	format(pivot_cte.InvoiceMonth, 'dd.MM.yyyy') as InvoiceMonth,
+	pivot_cte.[Gasport, NY],
+	pivot_cte.[Jessie, ND],
+	pivot_cte.[Medicine Lodge, KS],
+	pivot_cte.[Peeples Valley, AZ],
+	pivot_cte.[Sylvanite, MT]
+from pivot_cte
+order by pivot_cte.InvoiceMonth
+
+
+/*
+2. Для всех клиентов с именем, в котором есть "Tailspin Toys"
+вывести все адреса, которые есть в таблице, в одной колонке.
+
+Пример результата:
+----------------------------+--------------------
+CustomerName                | AddressLine
+----------------------------+--------------------
+Tailspin Toys (Head Office) | Shop 38
+Tailspin Toys (Head Office) | 1877 Mittal Road
+Tailspin Toys (Head Office) | PO Box 8975
+Tailspin Toys (Head Office) | Ribeiroville
+----------------------------+--------------------
+*/
+
+select
+	Customers.CustomerName,
+	cCustomers.Address as AddressLine
+from Sales.Customers as Customers
+CROSS APPLY 
+	(
+		select	C.DeliveryAddressLine1 as Address from Sales.Customers as C where C.CustomerID = Customers.CustomerID
+		union all 
+		select	C.DeliveryAddressLine2 as Address from Sales.Customers as C where C.CustomerID = Customers.CustomerID
+		union all 
+		select	C.PostalAddressLine1 as Address from Sales.Customers as C where C.CustomerID = Customers.CustomerID
+		union all 
+		select	C.PostalAddressLine2 as Address from Sales.Customers as C where C.CustomerID = Customers.CustomerID
+	) as cCustomers
+where Customers.CustomerName like 'Tailspin Toys%'
+order by 1
+
+
+/*
+3. В таблице стран (Application.Countries) есть поля с цифровым кодом страны и с буквенным.
+Сделайте выборку ИД страны, названия и ее кода так, 
+чтобы в поле с кодом был либо цифровой либо буквенный код.
+
+Пример результата:
+--------------------------------
+CountryId | CountryName | Code
+----------+-------------+-------
+1         | Afghanistan | AFG
+1         | Afghanistan | 4
+3         | Albania     | ALB
+3         | Albania     | 8
+----------+-------------+-------
+*/
+
+select 
+Countries.CountryID,
+Countries.CountryName,
+cCountries.Code
+from Application.Countries as Countries
+cross apply
+	(
+		select	C.IsoAlpha3Code as Code from Application.Countries as C where C.CountryID = Countries.CountryID
+		union all 
+		select	CAST(C.IsoNumericCode as CHAR(10)) from Application.Countries as C where C.CountryID = Countries.CountryID
+	) as cCountries
+order by 1, 3 desc
+
+/*
+4. Выберите по каждому клиенту два самых дорогих товара, которые он покупал.
+В результатах должно быть ид клиета, его название, ид товара, цена, дата покупки.
+*/
+
+select distinct
+	Invoices_1.CustomerID,
+	Customers_1.CustomerName as CustomerName,
+	StockItems_1.StockItemID,
+	StockItems_1.StockItemName,
+	StockItems_1.UnitPrice,
+	StockItems_1.InvoiceDate
+from Sales.Invoices as Invoices_1
+left join Sales.Customers as Customers_1
+	on Invoices_1.CustomerID = Customers_1.CustomerID
+CROSS APPLY 
+	(
+		select distinct top 2 
+		max(Invoices.InvoiceDate) over (partition by Invoices.CustomerID, StockItems.StockItemID order by Invoices.InvoiceDate desc) InvoiceDate,
+		StockItems.StockItemID,
+		StockItems.StockItemName,
+		StockItems.UnitPrice
+		from Sales.Invoices as Invoices
+		inner join Sales.InvoiceLines as InvoiceLines
+			on Invoices.InvoiceID = InvoiceLines.InvoiceID
+		inner join Warehouse.StockItems as StockItems
+			on InvoiceLines.StockItemID = StockItems.StockItemID
+		where Invoices.CustomerID = Invoices_1.CustomerID
+		ORDER BY StockItems.UnitPrice DESC
+	) AS StockItems_1
+order by 1, 3 desc, 5 desc
