8f887f5b4cddc194474a0c693af3b460fda113b4
SergeiGorshenin <gorsheninsn@mail.ru>
2024-09-08 23:39:24 +0300

    2
    2

--- HW12 - What indexes do you need/HW12.sql
+++ HW12 - What indexes do you need/HW12.sql
@@ -6,3 +6,21 @@ DROP INDEX IF EXISTS [FK_PointSaleProducts_PointSaleID] ON [dbo].[PointSaleProdu
 CREATE NONCLUSTERED INDEX [FK_PointSaleProducts_PointSaleID] 
 ON [dbo].[PointSaleProducts]([PointSaleID])
 GO
+
+DROP INDEX IF EXISTS [FK_Buyers_FullName] ON [dbo].Buyers
+
+CREATE NONCLUSTERED INDEX [FK_Buyers_FullName] 
+ON [dbo].Buyers(FullName)
+GO
+
+DROP INDEX IF EXISTS [FK_Sellers_Name] ON [dbo].Sellers
+
+CREATE NONCLUSTERED INDEX [FK_Sellers_Name] 
+ON [dbo].Sellers(Name)
+GO
+
+DROP INDEX IF EXISTS [FK_PointSaleProducts_Name] ON [dbo].ProductStandards
+
+CREATE NONCLUSTERED INDEX [FK_PointSaleProducts_Name] 
+ON [dbo].ProductStandards(Name)
+GO
